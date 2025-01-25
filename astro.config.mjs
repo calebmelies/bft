de1eefd2d64a1cb94e31f8d04dedcef45e5212dc
calebmelies <calebmelies@gmail.com>
2025-01-25 00:57:09 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://feature-tour.netlify.app',
